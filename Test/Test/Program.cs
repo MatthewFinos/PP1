6b4771f53cbeb6e0ba64dc11f78dc9736d044379
Kieren Black (96001841) <BLACKKK1@student.op.ac.nz>
2018-09-19 13:35:24 +1200

    kierens bit

--- Test/Test/Program.cs
+++ Test/Test/Program.cs
@@ -13,11 +13,8 @@ namespace Test
             /* will it work*/
             /* yes it will */
             /*Checking*/
-<<<<<<< HEAD
             /*Kieren code*/
-=======
             /*Matthew Code*/
->>>>>>> refs/remotes/origin/master
         }
     }
 }
